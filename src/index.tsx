7870ed801f787f2ca9adf28a2843a4ba2e5f63f0
Vira <35749970+Vestanra@users.noreply.github.com>
2024-11-25 13:59:08 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/",
-    element: <Layout />,
+    element: <PrivateRoute children={<Layout />} />,
     children: [
       {
         index: true,
